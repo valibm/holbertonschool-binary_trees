da4738e0ef6f315883d31ecb04ffa257a81068e1
root <valibm>
2024-01-13 22:28:46 -0800

    Handled betty Error

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -5,7 +5,8 @@
  *                            the right-child of another node.
  * @parent: Pointer to the node to insert the right-child.
  * @value: Value to store in the new node.
- * Return: Pointer to the created node, or NULL on failure or if parent is NULL.
+ * Return: Pointer to the created node, or NULL on failure
+ *         or if parent is NULL.
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
